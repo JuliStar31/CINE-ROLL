0732068ffead99781e9860c0e246b4a42f414c6c
Alvin <alvinckn2@gmail.com>
2026-08-08 07:09:09 +0700

    Add Laravel project files

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,11 +8,6 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function showLoginForm()
-    {
-        return view('auth.login');
-    }
-
     public function login(Request $request)
     {
         $credentials = $request->validate([
